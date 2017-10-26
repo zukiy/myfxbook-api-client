e28ba4a19bc81de282ba8bbdada2b0ad5c31e139
Mitroshin Evgeniy <e.mitroshin@olymptrade.com>
2017-10-26 15:32:40 +0300

    Login, Logout methods

--- client.go
+++ client.go
@@ -1,1 +1,44 @@
-package myfxbook_api_client
+package client
+
+// MyFxBookClient struct
+type MyFxBookClient struct {
+	Email    string
+	Password string
+	Session  string
+}
+
+// NewClient create new myfxbook client
+func NewClient(email, password string) *MyFxBookClient {
+	return &MyFxBookClient{
+		Email:    email,
+		Password: password,
+	}
+}
+
+// Login to account
+func (c *MyFxBookClient) Login() error {
+	var err error
+
+	loginResponseContainer := new(LoginResponse)
+	err = callGetRequest(getLoginURL(c.Email, c.Password), loginResponseContainer)
+	if err != nil {
+		return err
+	}
+
+	c.Session = loginResponseContainer.Session
+
+	return err
+}
+
+// Logout from account
+func (c *MyFxBookClient) Logout() error {
+	var err error
+
+	logoutResponseContainer := new(Response)
+	err = callGetRequest(getLogoutURL(c.Session), logoutResponseContainer)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
